ac0e475c7c8803c35096b3555f39f2b0cf853e47
GaboDev <gabrie5887@gmail.com>
2026-02-12 18:14:42 -0500

    forth commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const helmet = require("helmet");
-const xss = require("xss-clean");
 const { body, param, validationResult } = require("express-validator");
 const sanitizeHtml = require("sanitize-html");
 
